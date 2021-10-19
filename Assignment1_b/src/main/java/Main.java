6d4e174dd20c1da4c6a7dea0043a1abf1afec13d
Chideraa Nnaemeka <chideraannaemeka@Chideraas-MacBook-Air.local>
2021-10-19 12:55:26 +0100

    changes

--- Assignment1_b/src/main/java/Main.java
+++ Assignment1_b/src/main/java/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import org.joda.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Main {
+
+    public static void main(String args[]){
+
+    }
 }
